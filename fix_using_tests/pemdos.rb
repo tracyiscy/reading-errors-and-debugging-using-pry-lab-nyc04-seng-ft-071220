215bd2c09fae9bfa00e6b58234de3cc9c11ba8cd
CHENYUN ZHANG <morningchenyun@gmail.com>
2020-07-14 19:40:33 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,10 +1,7 @@
 # don't forget to add: require 'pry'
-require 'pry'
 def snake_it_up(string)
   if string[0] == "s"
-    binding.pry
     p "s"*10 + string
-    bining.pry
   else
     string
   end
